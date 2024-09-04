1766661c5228b55d58004e818e96ed79912f808d
Stan <stanedgar@gmail.com>
2024-09-03 19:26:00 -0600

    Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let text = string.split();
+    text.reverse();
+    let Finish = text.toString();
+    
+    
+    
+    return Finish;
+    
+    };
+    
+    console.log(reverseString("john"));
 
 // Do not edit below this line
 module.exports = reverseString;
