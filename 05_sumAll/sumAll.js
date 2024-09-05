dfcc717f1d8628b0504702b3ee244ae30b89ee3e
Stan <stanedgar@gmail.com>
2024-09-04 19:55:44 -0600

    a BREEze

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+// Adds numbers to an array
+    let finalSum = []
+for (num1; num1 <= num2; num1++){
+
+    finalSum.push(num1);
+}
+
+let sum = 0;
+
+for (i = 0; i < finalSum.length; i++ ){
+    sum += finalSum[i];
+}
+
+
+console.log(sum);
+
 
 };
+
+console.log(sumAll(1, 4));
+
+// get numbers
+// make a variable to contain
+
 
 // Do not edit below this line
 module.exports = sumAll;
