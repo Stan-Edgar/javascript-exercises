8b42436eb443edc9bdc75786083a289fa0941b18
Stan <stanedgar@gmail.com>
2024-09-04 20:07:08 -0600

    DONE

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+let Fahrenheit = 32;
+let answer = num * Fahrenheit;
+return answer;
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(4));
+
+const convertToFahrenheit = function(num) {
+  let Celsius = -32;
+let answer = num + Celsius;
+return answer;
 };
+
+console.log(convertToFahrenheit(64));
+
 
 // Do not edit below this line
 module.exports = {
