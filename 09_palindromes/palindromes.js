a38616efa38844d26a1e95b53160e26497472456
Stan <stanedgar@gmail.com>
2024-10-23 19:41:58 -0600

    On A ROLL

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+function palindromes(word) {
 
-};
+   let final = [...word].filter(char => /[^\W_]/.test(char)).join('').toLowerCase();
+   const strRev =  final.trim().split('').reverse().join('').toLowerCase();
+
+
+
+console.log(final)
+
+       if (strRev === final) {
+           console.log("true")}
+           else {
+            console.log("false")
+           };
+       
+   };
+   
+palindromes("Animal loots foliated detail of stool lamina");
+
 
 // Do not edit below this line
 module.exports = palindromes;
