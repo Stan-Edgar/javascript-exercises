7903c6a36f293ecd062e958c5672b0160631529f
Stan <stanedgar@gmail.com>
2024-10-23 19:13:34 -0600

    After months, i've conquered it with deep work

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,84 @@
-const add = function() {
+const add = function(num1, num2, ...args) {
+
+
+
+  let result = num1 + num2 ;
+  return result;
 	
 };
 
-const subtract = function() {
+
+
+const subtract = function(num1, num2, ...args) {
+	
+  let result = num1 - num2 ;
+  return result;
 	
 };
 
-const sum = function() {
+
+
+const sum = function(num1, num2) {
+  let finalSum = []
+  for (num1; num1 <= num2; num1++){
+  
+      finalSum.push(num1);
+  }
+  
+  let Sum = 0;
+  
+  for (i = 0; i < finalSum.length; i++ ){
+      Sum += finalSum[i];
+  }
+
+  return Sum;
+
+};
+
+
+
+const multiply = function(num1, num2) {
+  let result = num1 * num2 ;
+  return result;
+	
+
+};
+
+
+const power = function(num1, num2) {
+  let result = num1 ** num2;
+  return result;
 	
 };
 
-const multiply = function() {
 
-};
 
-const power = function() {
-	
-};
+const factorial = function(num1) {
+  //Creates an array that decreases the argument to 1.
+    let finalFact = [];
+    let origin = num1;
+    for (i = 1; num1 > i; num1--){
+      finalFact.push(num1);  
+    };
 
-const factorial = function() {
-	
-};
+
+    // ----------------------------------------------->
+
+    let length = finalFact.length;
+
+
+    for (i = 1; length > i ; i++){
+
+      origin *= finalFact[i];
+      
+      console.log(origin);
+
+
+    };
+
+  };
+
+factorial(10); 
 
 // Do not edit below this line
 module.exports = {
