b7a713a4dc876c8032575c62edcfc1d034d3f0a8
Stan <stanedgar@gmail.com>
2024-10-28 14:12:23 -0600

    DONE IN CLASS

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+let def = [0, 1];
+
+let first = def[def.length - 1];
+let second = def[def.length -2];
+
+
+for (i = 0; num > i; i++) {
+
+
+    let first = def[def.length - 1];
+    let second = def[def.length -2];
+
+let result = first + second;
+def.push(result);
+
+
+}
+
+console.log(def[num])
 
 };
+
+fibonacci(6);
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,26 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Deep Work',
+    author: 'Call Newport'
+  },
+  {
+    title: 'Boron Letters',
+    author: 'Gary Halbert'
+  }
+]
+
+
+const getTheTitles = function(books) {
+
+for (i = 0; books.length > i; i++) {
+  console.log(books[i].title);
+}
 
 };
+
+getTheTitles(books);
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
