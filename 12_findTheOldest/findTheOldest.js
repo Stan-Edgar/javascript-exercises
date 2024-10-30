24f750e00facc314756d208d32902dffe90554ac
Stan <stanedgar@gmail.com>
2024-10-29 20:28:21 -0600

    IT WORKED WOOOOOH

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,48 @@
-const findTheOldest = function() {
+const date = new Date();
+
+const people = [
+    {
+      name: 'Alexander the great',
+      birthYear: 1896,
+      deathYear: date.getFullYear(),
+      
+    },
+    {
+        name: 'Joey',
+        birthYear: 1999,
+        deathYear: date.getFullYear(),
+        
+    }
+  ]
+
+  
+
+const findTheOldest = function(people) {
+
+let numbers = [];
+
+
+for (i = 0; people.length > i; i++) {
+
+  let age = people[i].deathYear - people[i].birthYear; // Finds out age
+  numbers.push(age) // push age into an array
 
 };
 
+for (i = 0; people.length > i; i++) {
+
+  let age = people[i].deathYear - people[i].birthYear; 
+  
+  if (Math.max(...numbers) === age) {
+  console.log(`${people[i].name}, ${Math.max(...numbers)}`) }; 
+}
+ 
+
+};
+
+
+
+
+findTheOldest(people);
 // Do not edit below this line
 module.exports = findTheOldest;
